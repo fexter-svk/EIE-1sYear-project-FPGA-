bdaba1f0161c8686544aba74ea69944ebc26115c
fexter-svk <ferianc.martin@gmail.com>
2016-03-20 15:06:48 +0000

    updated main code

--- hl_simulation/Python/main.py
+++ hl_simulation/Python/main.py
@@ -34,6 +34,19 @@ def CleanImage(image):
 
     return image
 
+def checkPixels(image, coordinates):
+    x_ref, y_ref = coordinates
+    pixels = image.load()
+    width, height = image.size
+    counter = 0
+    for x in range(x_ref-5, x_ref+5):
+        for y in range(y_ref-5,y_ref+5):
+            if (x>=0) and (x<width) and (y>=0) and (y<height):
+                RGB = pixels[x,y]
+                if(RGB[0]==0) and (RGB[1]==0) and (RGB[2]==0):
+                    counter+=1
+    return counter
+
 def TopPoint(image):
     width, height = image.size
     pixels = image.load()
@@ -73,77 +86,75 @@ def MiddlePoint(image, top, height):
 def WaistPoint(image, top, height):
     x_top,y_top = top
     x_waist = x_top
-    y_waist = y_top + round(height*0.618)
+    y_waist = y_top + round(height*0.518)
     return (x_waist, y_waist)
 
 def LeftArmPoint(image, top, height):
     width, height = image.size
     pixels = image.load()
-    y_max = 0
-    x_min = width
     x_top,y_top = top
-    for x in range(0,x_top):
-        for y in range(0, y_top + round(height*0.618)):
+    y_result = 0
+    x_result = x_top
+    for y in range(0, y_top + round(height*0.618)):
+        for x in range(0, x_top):
             RGB = pixels[x,y]
             if(RGB[0]==0) and (RGB[1]==0) and (RGB[2]==0):
-                if(x<x_top*6) and (y<y_top*6):
-                     if(x<x_min):
-                        y_max = y
-                        x_min = x
-    return (x_min, y_max)
+                if (x_result>x) and (y_result<y) and (20<(checkPixels(image, (x,y)))):
+                    y_result = y
+                    x_result = x
+    return (x_result, y_result)
 
 def RightArmPoint(image, top, height):
     width, height = image.size
     pixels = image.load()
-    y_max = 0
-    x_min = 0
     x_top,y_top = top
-    for x in range(x_top, width):
-        for y in range(0, y_top + round(height*0.618)):
+    y_result = 0
+    x_result = x_top
+    for y in range(0, y_top + round(height*0.618)):
+        for x in range(x_top, width):
             RGB = pixels[x,y]
             if(RGB[0]==0) and (RGB[1]==0) and (RGB[2]==0):
-                if(x<x_top*6) and (y<y_top*6):
-                     if(x>x_min):
-                        y_max = y
-                        x_min = x
-    return (x_min, y_max)
+                if (x_result<x) and (y_result<y) and (20<(checkPixels(image, (x,y)))):
+                    y_result = y
+                    x_result = x
+    return (x_result, y_result)
 
 def LeftLegPoint(image, top):
     width, height = image.size
     pixels = image.load()
-    y_max = 0
-    x_min = width
+    y_result = 0
+    x_result = 0
     x_top,y_top = top
-    for x in range(width-1,0,-1):
-        for y in range(height-1,0,-1):
+    for x in range(0,x_top):
+        for y in range(height-1,y_top,-1):
             RGB = pixels[x,y]
             if(RGB[0]==0) and (RGB[1]==0) and (RGB[2]==0):
-                if(x<x_top*6) and (y>y_top*6):
-                     if(x<x_min):
-                        y_max = y
-                        x_min = x
-    return (x_min, y_max)
+                if (y_result<y) and (x_result<x) and (20<(checkPixels(image, (x,y)))):
+                    y_result = y
+                    x_result = x
+    return (x_result, y_result)
+
 
 def RightLegPoint(image, top):
     width, height = image.size
     pixels = image.load()
-    y_max = 0
-    x_min = 0
+    y_result = 0
+    x_result = width
     x_top,y_top = top
-    for x in range(width-1,0,-1):
-        for y in range(height-1,0,-1):
+    for x in range(width-1,x_top,-1):
+        for y in range(height-1,y_top,-1):
             RGB = pixels[x,y]
             if(RGB[0]==0) and (RGB[1]==0) and (RGB[2]==0):
-                if(x<x_top*1) and (y>y_top*6):
-                     if(x>x_min):
-                        y_max = y
-                        x_min = x
-    return (x_min, y_max)
+                if (y_result<y) and (x_result>x) and (20<(checkPixels(image, (x,y)))):
+                        y_result = y
+                        x_result = x
+    return (x_result, y_result)
 
 def drawCircle(image, coordinates):
     x,y = coordinates
     draw = ImageDraw.Draw(image)
     draw.ellipse((x-15, y-15, x+15, y+15), fill=(255,0,0))
+
 
 
 def drawLine(image,coordinates1, coordinates2):
@@ -154,20 +165,27 @@ def drawLine(image,coordinates1, coordinates2):
 
 
 def main():
-    ref_image = Image.open("ref_image2.jpg")
-    body_image = Image.open("body_image2.jpg")
+    ref_image = Image.open("ref5.jpg")
+    ref_image.save('r1.bmp')
+    body_image = Image.open("body5.jpg")
+    body_image.save('b1.bmp')
 
     ref_image = BW(ref_image)
     ref_image = Find_Edges(ref_image)
     ref_image = Invert(ref_image)
+    ref_image.save('r2_edge.bmp')
+
 
     body_image = BW(body_image)
     body_image = Find_Edges(body_image)
     body_image = Invert(body_image)
+    body_image.save('b2_edge.bmp')
+
 
     diff = ImageChops.difference(ref_image, body_image)
     diff = Invert(diff)
     diff = CleanImage(diff)
+    diff.save('d1.bmp')
 
     x_top, y_top = TopPoint(diff)
     x_leftLeg, y_leftLeg = LeftLegPoint(diff, (x_top, y_top))
@@ -197,7 +215,8 @@ def main():
     drawCircle(diff, (x_leftArm, y_leftArm))
     drawCircle(diff, (x_middle, y_middle))
     drawCircle(diff, (x_waist, y_waist))
-    diff.show()
+    diff.save('d2.bmp')
+    #diff.show()
 
 if __name__=="__main__":
     main()
