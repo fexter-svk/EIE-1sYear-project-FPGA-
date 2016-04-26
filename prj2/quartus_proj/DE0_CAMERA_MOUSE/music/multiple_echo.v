365b8062ce363e05f68dbf78eba99e266eb8c8e7
unknown <gsp14@eews104a-011.ic.ac.uk>
2016-04-26 16:05:00 +0100

    sound and video working

--- prj2/quartus_proj/DE0_CAMERA_MOUSE/music/multiple_echo.v
+++ prj2/quartus_proj/DE0_CAMERA_MOUSE/music/multiple_echo.v
@@ -8,15 +8,14 @@
 
 module processor (sysclk, data_in, data_valid, data_out,volume);
 
-	input				sysclk;		// system clock
+		input				sysclk;		// system clock
 	input [9:0]		data_in;		// 10-bit input data
 	input 			data_valid;
-	input [7:0]		volume;
 	output [9:0] 	data_out;	// 10-bit output data
-
+	input [7:0] volume;
 	wire				sysclk;
 	wire [9:0]		data_in;
-	//reg [9:0] 		data_out;
+	reg [9:0] 		data_out;
 	wire [9:0]		x,y;
 	wire 				pulse;
 	wire 				d; // wire from full output to flip flop
@@ -31,10 +30,36 @@ module processor (sysclk, data_in, data_valid, data_out,volume);
 	
 	pulse_gen pulse_gen(pulse, data_valid, sysclk);
 	
-	assign y = data_in[9:0] - ADC_OFFSET;		// x is input in 2's complement
+	assign x = data_in[9:0] - ADC_OFFSET;		// x is input in 2's complement
 	
-	assign data_out = (y  + DAC_OFFSET) * 4;
+	// This part should include your own processing hardware 
+	// ... that takes x to produce y
+	// ... In this case, it is ALL PASS.
+	//assign y = 4*x;
+	
+	FIFO FIFO (
+	.clock(sysclk),
+	.data(y),
+	.rdreq(and_out),
+	.wrreq(pulse),
+	.full(d),
+	.q(q));
+	
+	assign and_out = pulse && ff_out;
+	
+	assign att_q[8:0] = q[9:1];
+	assign att_q[9] = q[9];
+	
+	assign y = x - att_q;
 	
 	
+	
+	//  Now clock y output with system clock
+	always @(posedge sysclk) begin
+		data_out <=  y + DAC_OFFSET;
+		
+		ff_out <= d;
+		
+	end
 		
 endmodule
