c1cad8263f143c58622b6b57c66accfe8c59ec2b
fexter-svk <ferianc.martin@gmail.com>
2016-05-07 10:36:56 +0100

    updated hl files and simulation

--- hl_files/main.py
+++ hl_files/main.py
@@ -1,50 +1,46 @@
 from __future__ import print_function
 from PIL import Image, ImageChops, ImageFilter, ImageOps, ImageDraw
 
-#LEFT (YELLOW)
-# R = 207 G = 220 B = 64
-#
-#RIGHT (GREEN)
-# R = 140 G = 225 B = 103
-#
-#
-def mainBlock(image,):
+def mainBlock(image):
     pixels = image.load()
     width, height = image.size
     x_right_arm, y_right_arm = (0,0)
     x_left_arm, y_left_arm = (0,0)
+    red_found = False
+    blue_found = False
     for y in range(height):
         for x in range(width):
             RGB = pixels[x,y]
-            if not (((80<=RGB[0]) and (RGB[0]<=145)) and ((150<=RGB[1]) and (RGB[1]<=230)) and ((30<=RGB[2]) and (RGB[2]<=108))):
-                if not (((170<=RGB[0]) and (RGB[0]<=222)) and ((185<=RGB[1]) and (RGB[1]<=235)) and ((49<=RGB[2]) and (RGB[2]<=149))):
-                    pixels[x,y] = (0,0,0)
-                else:
-                    if (x_right_arm==0) and (y_right_arm==0):
-                        x_right_arm = x
-                        y_right_arm = y
-                    #GREEN
-            else:
-                if (x_left_arm==0) and (y_left_arm==0):
-                    x_left_arm = x
-                    y_left_arm = y
-                pass
-                #YELLOW
-    if (x_right_arm != 0) and (y_right_arm != 0) and (x_left_arm != 0) and (y_left_arm!=0):
-        volume = abs(round((y_right_arm+y_left_arm/2)-240))
-        volume = volume // 80
-    return (image, volume)
+            if (((0<=RGB[0]) and (RGB[0]<=100)) and ((0<=RGB[1]) and (RGB[1]<=100)) and ((100<=RGB[2]) and (RGB[2]<=255)) and not blue_found):
+                blue_found = True
+                x_right_arm = x
+                y_right_arm = y
+            if (((200<=RGB[0]) and (RGB[0]<=255)) and ((0<=RGB[1]) and (RGB[1]<=80)) and ((0<=RGB[2]) and (RGB[2]<=80)) and not red_found):
+                red_found = True
+                x_left_arm = x
+                y_left_arm = y
+            pixels[x,y] = (0,0,0)
+
+    for x in range(x_right_arm, x_right_arm + 40):
+        for y in range(y_right_arm, y_right_arm + 40):
+            if (x<width and y<height):
+                pixels[x,y] = (0,0,255)
+
+    for x in range(x_left_arm, x_left_arm + 40):
+        for y in range(y_left_arm, y_left_arm + 40):
+            if (x<width and y<height):
+                pixels[x,y] = (255,0,0)
+
+    return image
 
 
 
 def main():
-    image = Image.open("ex1.jpg")
-    result = tuple()
-    result = mainBlock(image)
-    result[0].show()
-    print(result[1])
-    x_left_arm, y_left_arm = (0,0)
-    x_right_arm, y_right_arm = (0,0)
+    image = Image.open("ex2.jpg")
+    image.show()
+    image = mainBlock(image)
+    image.show()
+    image.save("detected2.jpg")
 
 
 
